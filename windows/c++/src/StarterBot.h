5f148ab53d5f9dc29562034f7400eecce054e68c
ksfarag <ksfarag@mun.ca>
2021-04-03 23:13:36 -0230

    uploaded the full project file

---
+++ windows/c++/src/StarterBot.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "MapTools.h"
+
+#include <BWAPI.h>
+
+class StarterBot
+{
+    MapTools m_mapTools;
+
+	int buildIndex = 0;
+	int initalNumberOfUnits;
+	std::string buildOrder[3] = {"Probe","Probe","Pylon"};
+	
+public:
+
+    StarterBot();
+
+    // helper functions to get you started with bot programming and learn the API
+    void sendIdleWorkersToMinerals();
+    void trainAdditionalWorkers();
+    void buildAdditionalSupply();
+    void drawDebugInformation();
+
+    // functions that are triggered by various BWAPI events from main.cpp
+	void onStart();
+	void onFrame();
+	void onEnd(bool isWinner);
+	void onUnitDestroy(BWAPI::Unit unit);
+	void onUnitMorph(BWAPI::Unit unit);
+	void onSendText(std::string text);
+	void onUnitCreate(BWAPI::Unit unit);
+	void onUnitComplete(BWAPI::Unit unit);
+	void onUnitShow(BWAPI::Unit unit);
+	void onUnitHide(BWAPI::Unit unit);
+	void onUnitRenegade(BWAPI::Unit unit);
+};
